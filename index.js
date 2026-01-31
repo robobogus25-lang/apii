c41d85e1f5d29d22c83d1afa455870bd8e6cb1e6
robobogus25-lang <robobogus25@gmail.com>
2026-01-31 18:38:00 +0200

    685

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+const url = require('url');
+const dataPath = path.join(__dirname, 'data')
+
 const server = HTMLOutputElement.createServer((req, res)=> {
     if(req.url == '/jokes' && req.method == 'GET') {
         getAllJokes(req, res);
